a9d3ec937dad7d2eb4bc2f46e0b7cdbfad5ec71e
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2023-03-28 12:53:49 +0300

    use case

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  ArcElement,
+  Tooltip,
+  Title,
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+} from 'chart.js';
+import { PolarArea } from 'react-chartjs-2';
+
+ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Title);
+
+export const data = {
+  labels: ['work', 'health', 'family', 'Personal growth', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of votes',
+      data: [12, 21, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.5)',
+        'rgba(255, 206, 86, 0.5)',
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(153, 102, 255, 0.5)',
+        'rgba(255, 159, 64, 0.5)',
+      ],
+      borderWidth: 2,
+    },
+  ],
+};
+export const plugins = {
+  title: {
+    display: true,
+    text: 'Chart.js Polar Area Chart With Centered Point Labels',
+  },
+}
+export const options = {
+  plugins: plugins,
+  responsive: true,
+  scales: {
+    r: {
+      pointLabels: {
+        display: true,
+        centerPointLabels: true,
+        font: {
+          size: 18
+        }
+      }
+    }
+  },
+  
 }
 
-export default App;
+export default function App() {
+  return <PolarArea data={data} options={options} />;
+}
